45c50f701acd6c85dfffa1dcd62c280765d180e6
PROXIMO <17911448+PROX1MO@users.noreply.github.com>
2019-11-16 15:50:32 +0200

    Update index.php

--- index.php
+++ index.php
@@ -60,7 +60,7 @@ define('USE_USERNAME', false);
 define('LOGOUT_URL', './');
 
 // time out after NN minutes of inactivity. Set to 0 to not timeout
-define('TIMEOUT_MINUTES', 10);
+define('TIMEOUT_MINUTES', 0);
 
 // This parameter is only useful when TIMEOUT_MINUTES is not zero
 // true - timeout time from last activity, false - timeout time from login
